7fef3ad2c44247fc8d4a664bdb88005f02d91cf3
Alex Woodman <alex@Alexs-MacBook-Pro-2.local>
2019-08-20 10:36:41 -0400

    validation

--- src/components/data/LogInForm/LogInForm.tsx
+++ src/components/data/LogInForm/LogInForm.tsx
@@ -37,10 +37,9 @@ const LogInForm: React.FC<LogInFormProps> = ({
   const [generalError, setGeneralError] = React.useState(false);
 
   const LoginSchema = Yup.object().shape({
-    email: Yup.string()
+    username: Yup.string()
       .min(4, "Too Short!")
       .max(100, "Too Long!")
-      .required("Required")
       .email(),
     password: Yup.string()
       .min(4, "Too Short!")
